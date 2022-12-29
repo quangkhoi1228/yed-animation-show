42aea09623102a27ed04598298d317e76e61703e
khoitq-acl <khoitq@americancodelab.com>
2022-12-29 15:20:14 +0700

    Add acl and mft section

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var main = {
+  logoDrawContainer: '',
+
+  init() {
+    console.log('a');
+    main.logoDrawInit();
+  },
+
+  logoDrawInit() {
+    let aclAndMftIframe = document.getElementById('aclAndMftIframe');
+    // aclAndMftIframe.setAttribute('src', '/pages/aclandmft.html');
+  }
+};
+
