00234eac3569f882b0448cb3b4bf0ddf3a4deca2
khoitq-acl <khoitq@americancodelab.com>
2022-12-31 15:25:31 +0700

    Add second typing text section

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,60 +24,6 @@ var main = {
   textTypingEffectInit() {
     var container = document.querySelector('.effect-typing-text-container');
     container.scrollIntoView()
-    var TxtType = function (el, toRotate, period) {
-      this.toRotate = toRotate;
-      this.el = el;
-      this.loopNum = 0;
-      this.period = parseInt(period, 10) || 2000;
-      this.txt = '';
-      this.tick();
-      this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function () {
-      var i = this.loopNum % this.toRotate.length;
-      if (this.loopNum < this.toRotate.length) {
-
-
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-          this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-          this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        // don't delete when last message
-        if (!(this.isDeleting && this.loopNum == this.toRotate.length - 1)) {
-
-          this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-        } else {
-          backgroundFirework();
-          main.countdownInit();
-
-        }
-        var that = this;
-        var delta = 150 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-          delta = this.period;
-
-
-          this.isDeleting = true;
-
-        } else if (this.isDeleting && this.txt === '') {
-          this.isDeleting = false;
-          this.loopNum++;
-          delta = 500;
-        }
-
-        setTimeout(function () {
-          that.tick();
-        }, delta);
-      }
-    };
 
     // window.onload = function () {
     var elements = document.getElementsByClassName('typewrite');
@@ -85,7 +31,10 @@ var main = {
       var toRotate = elements[i].getAttribute('data-type');
       var period = elements[i].getAttribute('data-period');
       if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
+        new TxtType(elements[i], JSON.parse(toRotate), period, function () {
+          backgroundFirework();
+          main.countdownInit();
+        });
 
       }
     }
@@ -144,7 +93,29 @@ var main = {
     button.onclick = () => main.secondTypingTextInit();
   },
   secondTypingTextInit() {
-    console.log('a')
+    var container = document.querySelector('.effect-typing-text-container.second');
+    container.scrollIntoView();
+    var elements = document.getElementsByClassName('typewrite1');
+    for (var i = 0; i < elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period, function () {
+          main.happyNewYearInit();
+        });
+
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite1 > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+    // };
+
+  },
+  happyNewYearInit() {
+
   },
 };
 
@@ -577,3 +548,60 @@ l114.86-111.955C511.559,208.648,513.031,202.687,511.266,197.258z`;
 
   });
 }
+
+var TxtType = function (el, toRotate, period, callback) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+  this.callback = callback
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  if (this.loopNum < this.toRotate.length) {
+
+
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // don't delete when last message
+    if (!(this.isDeleting && this.loopNum == this.toRotate.length - 1)) {
+
+      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+    } else {
+      this.callback();
+      // backgroundFirework();
+      // main.countdownInit();
+
+    }
+    var that = this;
+    var delta = 150 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+
+
+      this.isDeleting = true;
+
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
+  }
+};
