be62ab0f61a9ffec7f0692f6cabb3137c4540b58
khoitq-acl <khoitq@americancodelab.com>
2023-01-06 16:15:21 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,12 +190,13 @@ var main = {
 
     let container = document.querySelector('.awards-video-container');
     let video = document.getElementById('awardsVideo');
-    video.src = 'https://drive.google.com/file/d/1mg6nu6QtFEih0VK7HxUb3G2G8Tj_HHy_/preview?autoplay=1'
-    let duration = (4 * 60 + 51) * 1000;
+    // video.src = 'https://drive.google.com/file/d/1mg6nu6QtFEih0VK7HxUb3G2G8Tj_HHy_/preview'
+    // let duration = (4 * 60 + 51) * 1000;
+    let duration = video.duration;
     setTimeout(() => {
       stopBackgroundFirework();
       container.scrollIntoView();
-      // video.play();
+      video.play();
       setTimeout(() => main.updateYearInit(), duration)
     }, 2000);
 
